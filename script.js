2756fd310e9113777aa5dd4859cfaebaed75dfa7
Harsh Vardhan Singh csy247561 <87150873+Harsh-Vardhan-Singh-202082@users.noreply.github.com>
2024-12-09 17:43:53 +0530

    Update script.js

--- script.js
+++ script.js
@@ -30,20 +30,20 @@ function checkCollision() {
             let rect2 = rectangles[j];
 
             if (
-                rect1.x < rect2.x + rect2.width &&
-                rect1.x + rect1.width > rect2.x &&
-                rect1.y < rect2.y + rect2.height &&
-                rect1.y + rect1.height > rect2.y
+                rect1.x > rect2.x + rect2.width ||
+                rect1.x + rect1.width < rect2.x ||
+                rect1.y > rect2.y + rect2.height ||
+                rect1.y + rect1.height < rect2.y
             ) {
-                rect1.borderColor = 'red';
-                rect1.fillColor= 'rgba(255, 116, 51, 0.6)';
-                rect2.borderColor = 'red';
-                rect2.fillColor= 'rgba(255, 116, 51, 0.6)';
-            } else {
                 rect1.borderColor = 'blue';
                 rect1.fillColor = 'rgba(173, 216, 230, 0.6)';
                 rect2.borderColor = 'blue';
                 rect2.fillColor = 'rgba(173, 216, 230, 0.6)';
+            } else {
+                rect1.borderColor = 'red';
+                rect1.fillColor= 'rgba(255, 116, 51, 0.6)';
+                rect2.borderColor = 'red';
+                rect2.fillColor= 'rgba(255, 116, 51, 0.6)';
             }
         }
     }
